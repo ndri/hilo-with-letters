76276e7f1e7f4c40a9d8a73381e1f5185b22ed3f
Andri <andri@tfwno.gf>
2014-08-10 16:25:44 +0300

    Update hilo.py

--- hilo.py
+++ hilo.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
-# Higher or Lower with base26
+# Higher or Lower with Letters
 import random
 
 alphabet = u"abcdefghijklmnopqrstuvwxyz"
 tries = 0
+wordlist = "/usr/share/dict/words"
 
 # Function to convert a base26 word into a decimal number
 def numerize(word):
@@ -17,11 +18,27 @@ def numerize(word):
         count += alphabet.index(letter) * combinations
     return count
 
-# Choose secret word
-secret = ''.join([random.choice(alphabet) for i in range(random.randint(1,7))])
+# Try to get word from the wordlist, otherwise choose a random combination
+try:
+    with open('/usr/share/dict/words') as f:
+        words = f.read().decode('utf-8').splitlines()
+    # Make sure every word is a legal word according to the alphabet
+    words = filter(lambda word: not any(letter not in alphabet for letter in word), words)
+    # Make sure there are some words left after the filter
+    if len(words) == 0:
+        raise
+    secret = random.choice(words)
+    print "Fetched the word from the wordlist!"
+except:
+    # If the wordlist doesn't exist, it chooses a random combination
+    print "Failed to get a word from the wordlist, choosing random combination instead."
+    secret = ''.join([random.choice(alphabet) for i in range(random.randint(2,7))])
+
 secretnumber = numerize(secret)
 
 # Start the game
+print "Good luck!"
+
 while 1:
     try:
         guess = raw_input("Guess a word: ")
